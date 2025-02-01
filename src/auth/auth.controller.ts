e8faf58ebfa68abb7c0d4507b592741d3008e332
candy <candy@example.com>
2025-02-01 23:39:39 +0700

    create jwt and insert jwt into request other

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { LocalAuthGuard } from './passport/local-auth.guard';
+import { JwtAuthGuard } from './passport/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // @Post('login')
+  // create(@Body() createAuthDto: CreateAuthDto) {
+  //   return this.authService.signIn(
+  //     createAuthDto.username,
+  //     createAuthDto.password,
+  //   );
+  // }
+
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signIn(
-      createAuthDto.username,
-      createAuthDto.password,
-    );
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/modules/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { comparePassword } from 'src/helpers/utils';
@@ -16,40 +10,35 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<any> {
+  async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-    if (!user) {
-      throw new NotFoundException(`No user found for email with ${email}`);
-    }
-
     const isPasswordValid = await comparePassword(pass, user.password);
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid password / username');
-    }
 
+    if (!user || !isPasswordValid) return null;
+    return user;
+  }
+
+  // async signIn(email: string, pass: string): Promise<any> {
+  //   const user = await this.usersService.findByEmail(email);
+  //   if (!user) {
+  //     throw new NotFoundException(`No user found for email with ${email}`);
+  //   }
+
+  //   const isPasswordValid = await comparePassword(pass, user.password);
+  //   if (!isPasswordValid) {
+  //     throw new UnauthorizedException('Invalid password / username');
+  //   }
+
+  //   const payload = { username: user.email, sub: user._id };
+  //   return {
+  //     accessToken: await this.jwtService.signAsync(payload),
+  //   };
+  // }
+
+  async login(user: any) {
     const payload = { username: user.email, sub: user._id };
     return {
-      accessToken: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
-  }
-
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
   }
 }
